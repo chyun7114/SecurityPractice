9ab59c1f5dfc229117c515365fb0dabb19f23086
chyun7114 <chyun7114@naver.com>
2025-01-16 15:15:35 +0900

    set: SecurityFilterChain 설정

--- Spring-oAuth2/src/main/java/com/spring/springoauth2/config/SecurityConfig.java
+++ Spring-oAuth2/src/main/java/com/spring/springoauth2/config/SecurityConfig.java
@@ -1,4 +1,42 @@
 package com.spring.springoauth2.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // 접근 권한 설정
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/error", "/webjars/**").permitAll() // 인증 없이 접근 가능
+                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                );
+
+        // 오류 제어 핸들러 설정
+        http.exceptionHandling(exception -> exception
+                .authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                ) // 인증 실패 시 401 반환
+        );
+
+
+        // oAuth2 로그인 설정
+        // 일단 로그인 페이지는 기본 페이지로
+        http.oauth2Login(oauth2 -> oauth2
+                .loginPage("/login") // 로그인 페이지 경로 설정
+        );
+
+        return http.build();
+    }
 }
