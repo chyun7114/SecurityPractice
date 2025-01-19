d6c2c37a372ce394e35ab9c36aa845999ac4d001
chyun7114 <chyun7114@naver.com>
2025-01-19 14:34:51 +0900

    feat: 프롲젝트 재설정

---
+++ Spring-oAuth2/src/main/java/com/spring/springoauth2/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.spring.springoauth2.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("SpringSecurity Swagger")
+                .description("oAuth2 로그인 과정 연습을 위한 swagger UI")
+                .version("1.0.0");
+    }
+}
