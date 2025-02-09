20e237de391616cb5174ee8346b34ca8add7baf9
chyun7114 <chyun7114@naver.com>
2025-02-09 14:50:31 +0900

    feat: spring container로의 전환

--- SpringBasic_Inflearn/src/main/java/com/hello/core/AppConfig.java
+++ SpringBasic_Inflearn/src/main/java/com/hello/core/AppConfig.java
@@ -8,22 +8,28 @@ import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
 import com.hello.core.order.OrderService;
 import com.hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 전반적인 애플리케이션의 설정을 관리하는 클래스
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
